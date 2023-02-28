a499acc30b858699af0e0dfe071f20252ecd496a
Natalia Cheban <114994552+natalia-0729@users.noreply.github.com>
2023-02-28 23:11:42 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(function(){
 
-  $(".menu, .footer__go-top, .footer__logo").on("click", "a", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
+  $('.menu, a[href*="#"]').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1500);
+    return false;
+  });
 
   $('.menu__btn, .menu a').on('click', function(){
     $('.menu__list').toggleClass('menu__list--active')
