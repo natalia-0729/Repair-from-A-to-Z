ef893f86e5d67b4ce4bd19ced7c6f6f4c5358e18
Natalia Cheban <114994552+natalia-0729@users.noreply.github.com>
2023-03-02 14:43:08 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,3 +12,34 @@ $(function(){
 });
 
 });
+
+const buttonElems = document. querySelectorAll('.price__btn');
+const modalElem = document. querySelector('.modal');
+
+modalElem.style.cssText = `
+display: flex;
+visibility: hidden;
+opacity: 0;
+transition: opacity 300ms ease-in-out;
+`;
+
+const closeModal = event => {
+  const target = event.target;
+  if (target === modalElem || target.closest('.modal__close')) {
+    modalElem.style.opacity = 0;
+
+    setTimeout(() => {
+      modalElem.style.visibility = 'hidden';
+    }, 300)
+  } 
+}
+
+const openModal = () => {
+  modalElem.style.visibility = 'visible';
+  modalElem.style.opacity = 1;
+};
+
+buttonElems.forEach(btn =>{
+  btn.addEventListener('click', openModal);
+});
+modalElem.addEventListener('click', closeModal);
